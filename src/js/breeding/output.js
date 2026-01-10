ecf0d6cf6f44ff2ec1699748bc17bc3279407779
PSNapier <arjones.tx@gmail.com>
2026-01-10 08:14:59 -0600

    feat: started pheno reader and tweaked matchy()

--- src/js/breeding/output.js
+++ src/js/breeding/output.js
@@ -87,13 +87,13 @@ function rollGenoBase() {
 
 	// roll XZ base
 	const basePattern = /(XX|XZ|ZX|ZZ)/;
-	const sireGenoMatch = matchy(sire.geno, basePattern)[0].split('');
-	const damGenoMatch = matchy(dam.geno, basePattern)[0].split('');
+	const sireGenoMatch = sire.geno.matchy(basePattern)[0].split('');
+	const damGenoMatch = dam.geno.matchy(basePattern)[0].split('');
 	output.xz = randomizer(punnetSquare(sireGenoMatch, damGenoMatch));
 
 	// roll special
-	const sireSpecialMatch = matchy(sire.geno, /S\+/)[0];
-	const damSpecialMatch = matchy(dam.geno, /S\+/)[0];
+	const sireSpecialMatch = sire.geno.matchy(/S\+/)[0];
+	const damSpecialMatch = dam.geno.matchy(/S\+/)[0];
 	output.special = randomizer(
 		punnetSquare([sireSpecialMatch, ''], [damSpecialMatch, ''])
 	);
@@ -152,6 +152,20 @@ function rollGenoCont() {
 		.join('/');
 }
 
+function phenoReader(geno) {
+	console.log(geno);
+
+	let output = [];
+
+	// base
+	for (const gene of dict.genesBase) {
+		if (geno.matchy(gene[1])) {
+			// TODO: implement phenotype reading logic
+		}
+	}
+	return output;
+}
+
 function generateBreedingOutput() {
 	items = new checkItems();
 	sire = new Parent('sire');
@@ -170,6 +184,8 @@ function generateBreedingOutput() {
 		critter.geno =
 			cont && cont.length > 0 ? `${base}/${cont}` : `${base}`;
 		critters.push(critter);
+		const pheno = phenoReader(critter.geno);
+		critter.pheno = pheno.length === 0 ? 'Pheno' : pheno;
 	}
 
 	const output = [];
