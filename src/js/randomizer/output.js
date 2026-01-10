bc4241c334179e20a03b58f85d7236c43ff8be38
PSNapier <arjones.tx@gmail.com>
2026-01-10 09:33:02 -0600

    feat: implemented phenoReader() into randomizer

--- src/js/randomizer/output.js
+++ src/js/randomizer/output.js
@@ -3,7 +3,28 @@ function generateRandomCritter() {
 		mutation: rng(100) <= 5 ? randomizer(dict.mutations) : '',
 		species: randomizer(dict.species),
 		sex: randomizer(['male', 'female']),
-		genoBase: randomizer(dict.genesBase.map((g) => g[1])),
+		genoBase: [
+			[
+				rng(100) <= 5 ? '+S' : '',
+				randomizer(dict.genesBase.map((g) => g[1])),
+			].join(''),
+			(() => {
+				const pool = [
+					...dict.genesLightShade.map((g) => g[1]),
+					...dict.genesModifiers.map((g) => g[1]),
+				];
+				const picked = [];
+				for (let i = 0; i < 3 && pool.length > 0; i++) {
+					const choice = randomizer(pool);
+					picked.push(choice);
+					const idx = pool.indexOf(choice);
+					if (idx !== -1) pool.splice(idx, 1);
+				}
+				// Remove duplicates if any (defensive, as above guarantees uniqueness)
+				const uniquePicked = Array.from(new Set(picked));
+				return uniquePicked.join('/');
+			})(),
+		],
 		genoCont: [
 			rng(100) <= 5 ? randomizer(dict.genesAppy.map((g) => g[1])) : '',
 			// Roll up to 5 times, each with random chance, and pick unique results
@@ -38,9 +59,15 @@ function generateRandomCritter() {
 		].filter(Boolean),
 	};
 
+	const base = critter.genoBase;
+	const cont = critter.genoCont;
+	const geno = cont && cont.length > 0 ? `${base}/${cont}` : `${base}`;
+	const pheno = phenoReader(geno);
+	const phenoString = pheno.length === 0 ? 'Pheno' : pheno;
+
 	const outputForm = `${critter.mutation ? `[${critter.mutation.capitalizeStr()}] ` : ''}${critter.species.capitalizeStr()} | ${critter.sex.capitalizeStr()}
-Pheno (Carried Pheno)
-${critter.genoBase}${critter.genoCont.length ? '/' + critter.genoCont.join('/') : ''}`;
+${phenoString}
+${geno.replace(/,/g, '/')}`;
 
 	return outputForm;
 }
